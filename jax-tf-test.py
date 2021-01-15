2fca2288bc6f7676506a8aa8406b1c7dd3fbd4d4
Daniel O'Hanlon <dpohalon@gmail.com>
2021-01-15 02:48:20 +0000

    Add timing benchmark. TODO: Device.

--- jax-tf-test.py
+++ jax-tf-test.py
@@ -37,12 +37,12 @@ def filter_novec(p_proj, C_proj, H, G, m):
 
     return p, C
 
-@tf.function
+@tf.function(experimental_compile = False, autograph = True)
 def filter_tf(p_proj, C_proj, H, G, m):
 
     HG = tf.transpose(H) @ G
 
-    # Innermost two axies must be 'matrix'
+    # Innermost two axes must be 'matrix'
     inv_C_proj = tf.linalg.inv(C_proj)
 
     C = tf.linalg.inv(inv_C_proj + HG @ H)
@@ -54,6 +54,8 @@ def filter_tf(p_proj, C_proj, H, G, m):
     return p, C
 
 if __name__ == '__main__':
+
+    import time
 
     n_batch = 1024
 
@@ -70,16 +72,37 @@ if __name__ == '__main__':
     m = mB[:, 0]
 
     p_f_tf, C_f_tf = filter_tf(pB, CB, H, G, mB)
+    start = time.perf_counter()
+    for i in range(100) : p_f_tf, C_f_tf = filter_tf(pB, CB, H, G, mB)
+    stop = time.perf_counter()
+    time_tf = (stop - start) / 100.
+
     p_f_v, C_f_v = filter_vec(pB, CB, H, G, mB)
+    start = time.perf_counter()
+    for i in range(100) : p_f_v, C_f_v = filter_vec(pB, CB, H, G, mB)
+    stop = time.perf_counter()
+    time_vec = (stop - start) / 100.
 
     p_f_nv, C_f_nv = filter_novec(p, C, H, G, m)
-
-    pprint(p_f_tf)
-    pprint(p_f_v)
-    pprint(p_f_nv)
+    start = time.perf_counter()
+    for i in range(100) : p_f_nv, C_f_nv = filter_novec(p, C, H, G, m)
+    stop = time.perf_counter()
+    time_novec = (stop - start) / 100.
 
     filter_vmap = jit(vmap(filter_novec, in_axes=(0, 0, None, None, 1)))
 
     p_f_av, C_f_av = filter_vmap(pB, CB, H, G, mB)
+    start = time.perf_counter()
+    for i in range(100) : p_f_av, C_f_av = filter_vmap(pB, CB, H, G, mB)
+    stop = time.perf_counter()
+    time_autovec = (stop - start) / 100.
 
-    pprint(p_f_av)
+    # pprint(p_f_av)
+    # pprint(p_f_tf)
+    # pprint(p_f_v)
+    # pprint(p_f_nv)
+
+    print('Auto vec', round(time_autovec * 1E3, 3))
+    print('TensorFlow', round(time_tf * 1E3, 3))
+    print('No vec', round(time_novec * 1E3, 3))
+    print('Manual vec', round(time_vec * 1E3, 3))
